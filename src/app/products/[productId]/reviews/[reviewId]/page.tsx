3ae8a99f11ca9effab1e6afcfa5844fd75454c27
Ruth Gicheha <60509305+Raiynegicheha@users.noreply.github.com>
2024-04-21 09:01:35 +0300

    Update page.tsx

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,9 @@
+import { notFound } from "next/navigation"
+
 export default function ReviewId({params}: any){
+    if (parseInt(params.reviewId) > 1000 ){
+        notFound();
+    }
     return (
         <>
             <h1>Review {params.reviewId} for product {params.productId}</h1>
