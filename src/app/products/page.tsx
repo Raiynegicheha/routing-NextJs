3e3f9c1ce04dee8ba1de71aaf45464a451851f6b
Ruth Gicheha <60509305+Raiynegicheha@users.noreply.github.com>
2024-04-21 08:40:39 +0300

    Create page.tsx

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+export default function Products(){
+    return (
+        <>
+            <h1>Products</h1>
+            <p>Products 1</p>
+            <p>Products 2</p>
+            <p>Products 3</p>
+        </>
+    )
+}
