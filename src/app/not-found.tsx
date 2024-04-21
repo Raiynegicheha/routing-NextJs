0921fed84e996e1e7658b3fab0d1bc10ccc21534
Ruth Gicheha <60509305+Raiynegicheha@users.noreply.github.com>
2024-04-21 09:01:42 +0300

    Create not-found.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound(){
+    return (
+        <>
+            <h1>Not found</h1>
+            <p>Could not find the requested resource
+                
+            </p>
+        </>
+    )
+}
